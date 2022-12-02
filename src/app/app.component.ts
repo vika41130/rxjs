cb34058448ade306cfdf3e1a8da7d79bf4993cbe
dung.phan <dung.phan@inventis.vn>
2022-12-02 17:19:59 +0700

    combineLatestAll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { take, map, combineLatestAll } from 'rxjs/operators';
+import { interval } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'rxjs';
+
+	source$: any;
+	example$: any;
+	result: any[] = [];
+	constructor() {
+		// emit every 1s, take 2
+		this.source$ = interval(1000).pipe(take(2));
+		// map each emitted value from source to interval observable that takes 5 values
+		this.example$ = this.source$.pipe(
+			map(val =>
+				interval(1000).pipe(
+					map(i => ` (${val} - ${i})`),
+					take(5)
+				)
+			)
+		);
+		this.example$
+			.pipe(combineLatestAll())
+			.subscribe((value: any) => {
+				this.result.push(value);
+				console.log(value);
+			});
+	}
+
+
 }

4182e19c19e7525c1298f06acfd50ced51a33c41
dung.phan <dung.phan@inventis.vn>
2022-12-09 17:16:11 +0700

    startWith, endWith, finalize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { of, interval, concat } from 'rxjs';
+import { of, endWith, finalize, startWith } from 'rxjs';
 
 @Component({
 	selector: 'app-root',
@@ -8,23 +8,21 @@ import { of, interval, concat } from 'rxjs';
 })
 export class AppComponent implements OnInit {
 
-	constructor() {
-	}
+	constructor() {}
 
 	ngOnInit(): void {
-		// concat(
-		// 	of(1, 2, 3),
-		// 	of(4, 5, 6),
-		// 	of(7, 8, 9)
-		// ).subscribe(val => console.log(val));
-
-		// when source never completes, any subsequent observables never run
-			concat(
-				interval(1000),
-				of('This', 'Never', 'Runs')
-			)
-			// log: 1,2,3,4.....
-			.subscribe(console.log);
+		const source$ = of(1, 2);
+		source$
+		.pipe(
+			startWith(0),
+			endWith(-3, -4), // emit on completion
+			// do action on complete
+			finalize(() => {
+				console.log('finalize');
+			})
+		)
+		.subscribe(val => {
+			console.log(val);
+		});
 	}
-
 }
